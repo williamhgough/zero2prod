d830e01b958145543848c75cff5f0f8f30afb6e8
William Gough <williamhenrygough@gmail.com>
2020-08-30 20:35:07 +0100

    refactor(app): split bin & lib

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    run(listener)?.await
 }
